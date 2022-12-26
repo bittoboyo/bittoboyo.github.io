e59eddd887b367fcb01db1ecc4f5a3adbeb49b84
bittoboyo <89293051+bittoboyo@users.noreply.github.com>
2022-12-26 13:39:42 +1100

    Updated Overall Page
    Added numerous stuff, and cleaned up project files

--- js/main.js
+++ js/main.js
@@ -12,25 +12,6 @@ renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-//OLD STARS
-// const starGeometry = new THREE.BufferGeometry();
-// const starMaterial = new THREE.PointsMaterial({
-//     color: 0xffffff
-// });
-
-// const starVertices = []
-// for (let i = 0; i < 10000; i++) {
-//     const x = (Math.random() - 0.5) * 2000;
-//     const y = (Math.random() - 0.5) * 2000;
-//     const z = -Math.random() * 2000;
-//     starVertices.push(x, y, z);
-// }
-
-// starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3))
-
-// const stars = new THREE.Points(starGeometry, starMaterial);
-// scene.add(stars);
-
 //NEW STARS (LIGHTSPEED EFFECT)
 let line_count = 1000;
 let geom = new THREE.BufferGeometry();
@@ -68,32 +49,30 @@ scene.add(lines);
 camera.position.z = 200;
 
 function animate() {
-    for (let line_index = 0; line_index < line_count; line_index++) {
-        va[2*line_index] += 0.005; //Increase the velocity by the acceleration
-        va[2*line_index+1] += 0.025;
+  for (let line_index = 0; line_index < line_count; line_index++) {
+    va[2*line_index] += 0.005; //Increase the velocity by the acceleration
+    va[2*line_index+1] += 0.025;
 
-        // pa[6*line_index]++ //x Start
-        // pa[6*line_index+1]++ //y
-        pa[6*line_index+2] += va[6*line_index]; //z
+    // pa[6*line_index]++ //x Start
+    // pa[6*line_index+1]++ //y
+    pa[6*line_index+2] += va[6*line_index]; //z
 
-        // pa[6*line_index+3]++; //x End
-        // pa[6*line_index+4]++; //y
-        pa[6*line_index+5] += va[2*line_index+1]; //z
+    // pa[6*line_index+3]++; //x End
+    // pa[6*line_index+4]++; //y
+    pa[6*line_index+5] += va[2*line_index+1]; //z
 
-        if (pa[6*line_index+5] > 200) {
-          var z = Math.random() * 200 - 100;
-          pa[6*line_index+2] = z;
-          pa[6*line_index+5] = z;
-          va[2*line_index] = 0;
-          va[2*line_index+1] = 0;
-        }
-      }
-      pos.needsUpdate = true;
-      renderer.clearColor();
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-
-    // renderer.render(scene, camera);
-    // requestAnimationFrame(animate);
+    if (pa[6*line_index+5] > 200) {
+      var z = Math.random() * 200 - 100;
+      pa[6*line_index+2] = z;
+      pa[6*line_index+5] = z;
+      va[2*line_index] = 0;
+      va[2*line_index+1] = 0;
+    }
+  }
+  pos.needsUpdate = true;
+  renderer.clearColor();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
 }
+
 animate();
