a243f2d512492dba2d2d02f9cfd861b5e827c40d
bittoboyo <89293051+bittoboyo@users.noreply.github.com>
2023-01-12 16:10:17 +1100

    Updated Project/Skills Page
    Now has photos (not fully done) with hyperlinks to other projects!
    
    Need to figure out issue with scrolling, should be done soon :>

---
+++ shaders/fragment.glsl.js
@@ -0,0 +1,13 @@
+export const fragmentShader = 
+`uniform sampler2D globeTexture;
+uniform float r;
+uniform float g;
+uniform float b;
+varying vec2 vertexUV;
+varying vec3 vertexNormal;
+void main() {
+    float intensity = 1.05 - dot(vertexNormal, vec3(0.0, 0.0, 1.0));
+    vec3 atmosphere = vec3(r, g, b) * pow(intensity, 1.5);
+
+    gl_FragColor = vec4(atmosphere + texture2D(globeTexture, vertexUV).xyz, 1.0);
+}`
---
+++ shaders/vertex.glsl.js
@@ -0,0 +1,8 @@
+export const vertexShader = 
+`varying vec2 vertexUV;
+varying vec3 vertexNormal;
+void main() {
+    vertexUV = uv;
+    vertexNormal = normalize(normalMatrix * normal);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}`
